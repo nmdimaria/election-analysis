9ff2e7ec9f9cb55e2f44390cfb418b80c8e29e96
nmdimaria <67925165+nmdimaria@users.noreply.github.com>
2020-09-30 16:14:00 -0500

    Add files via upload

--- PyPoll.py
+++ PyPoll.py
@@ -6,6 +6,16 @@ import os
 file_to_load = os.path.join("Resources", "election_results.csv")
 # Create a filename variable to a direct or indirect path to the file.
 file_to_save = os.path.join("Analysis", "election_analysis.txt")
+
+#Total votes variable
+total_votes = 0
+
+# Candidate Options list
+candidate_options = []
+
+# Dictonary for candidate votes
+candidate_votes = {}
+
 with open(file_to_load) as election_data:
 
 # Read the file object with the reader function.
@@ -13,6 +23,22 @@ with open(file_to_load) as election_data:
 # Read and print the header row.
     headers = next(file_reader)
     print(headers)
+# Print each row in the CSV file.
+    for row in file_reader:
+        total_votes += 1
+#Candidate list
+        candidate_name = row[2]
+# Add the candidate name to the candidate list
+        if candidate_name not in candidate_options:
+            candidate_options.append(candidate_name)
+        # Begin tracking that candidate's vote count.
+            candidate_votes[candidate_name] = 0
+        # Add a vote to that candidate's count
+        candidate_votes[candidate_name] += 1
+for candidate_name in candidate_votes:
+    votes = candidate_votes[candidate_name]
+    vote_percentage = float(votes)/float(total_votes)*100
+    print(f"{candidate_name}: received {round(vote_percentage, 2)}% of the vote.")
 
 #Data needed
 #1 Total number of votes cast
